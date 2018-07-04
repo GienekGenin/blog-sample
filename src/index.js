de41613e5c3a4d35b099afa32baf428fe289e5fb
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-04 09:57:30 +0200

    Remaking hamburgers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import $ from 'jquery';
 import './scss/style.scss';
 
 $(document).ready(function(){
-    $('.menu-trigger').click(function(){
-        $('header ul').slideToggle(780);
-    });
-    $(window).resize(function(){
-        if ($(window).width() > 780 ) {
-            $('header ul').removeAttr('style');
-        }
-    });
+
 });
