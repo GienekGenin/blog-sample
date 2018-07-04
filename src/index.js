ad602e7b6a78f2b3eacaac409480107163a90ab6
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-04 13:09:39 +0200

    Working on main version

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import $ from 'jquery';
 import './scss/style.scss';
 
 $(document).ready(function(){
-
+    $('.menu-trigger').click(function(){
+        $('header ul').slideToggle(780);
+    });
+    $(window).resize(function(){
+        if ($(window).width() > 780 ) {
+            $('header ul').removeAttr('style');
+        }
+    });
 });
